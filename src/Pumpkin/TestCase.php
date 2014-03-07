f1dcaea26a6496cadca4adb6ec013b1d531f2ec1
Raphaël Lefebvre <raphael@raphaellefebvre.be>
2014-03-07 20:25:58 +0100

    init connection

--- src/Pumpkin/TestCase.php
+++ src/Pumpkin/TestCase.php
@@ -9,5 +9,49 @@ namespace Pumpkin;
 abstract class TestCase extends \PHPUnit_Extensions_Database_TestCase
 {
 
+    /**
+     * main db connection.
+     *
+     * @var \PHPUnit_Extensions_Database_DB_DefaultDatabaseConnection
+     */
+    private static $connection;
 
+    /**
+     * {@inheritDoc}
+     *
+     * @return \PHPUnit_Extensions_Database_DB_DefaultDatabaseConnection
+     */
+    protected function getConnection()
+    {
+        if (null === self::$connection) {
+            $this->setConnection($this->buildConnection());
+        }
+        return self::$connection;
+    }
+
+    /**
+     * Setter of $connection.
+     *
+     * @param \PHPUnit_Extensions_Database_DB_DefaultDatabaseConnection $connection
+     */
+    private function setConnection(\PHPUnit_Extensions_Database_DB_DefaultDatabaseConnection $connection)
+    {
+        self::$connection = $connection;
+    }
+
+    /**
+     * Builder of connection.
+     *
+     * @return \PHPUnit_Extensions_Database_DB_DefaultDatabaseConnection
+     */
+    private function buildConnection()
+    {
+        return $this->createDefaultDBConnection(
+            new \PDO(
+                $GLOBALS['db_dsn'],
+                $GLOBALS['db_username'],
+                $GLOBALS['db_password']
+            )
+        );
+    }
 }
