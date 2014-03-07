1f9328d03855be4590439f69c33df9f0675230af
Raphaël Lefebvre <raphael@raphaellefebvre.be>
2014-03-07 20:21:30 +0100

    init TestCase

---
+++ src/Pumpkin/TestCase.php
@@ -0,0 +1,13 @@
+<?php
+namespace Pumpkin;
+
+/**
+ * Class TestCase
+ * @package Pumpkin
+ * @author Raphaël Lefebvre <raphael@raphaellefebvre.be>
+ */
+abstract class TestCase extends \PHPUnit_Extensions_Database_TestCase
+{
+
+
+}
