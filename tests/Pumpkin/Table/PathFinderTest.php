93d2fd5bd48fc59d21ed246b21d64c8e057b2d9c
Raphaël Lefebvre <raphael@raphaellefebvre.be>
2014-05-24 23:30:37 +0200

    init PathFinder

---
+++ tests/Pumpkin/Table/PathFinderTest.php
@@ -0,0 +1,75 @@
+<?php
+namespace Pumpkin\Table;
+
+use Pumpkin\Test;
+use TRex\Reflection\MethodReflection;
+
+/**
+ * Class PathFinderTest
+ * @package Pumpkin\Table
+ * @author Raphaël Lefebvre <raphael@raphaellefebvre.be>
+ */
+class PathFinderTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Tests the recovery of the test dir path.
+     */
+    public function testGetTestDirPath()
+    {
+        $pathFinder = new PathFinder($this->getTable('tableName'));
+        $this->assertSame(dirname(__FILE__), $pathFinder->getTestDirPath());
+    }
+
+    /**
+     * Tests when there is a file.
+     */
+    public function testFindDataPathWithFileFounded()
+    {
+        $pathFinder = new PathFinder($this->getTable('tableName'));
+        $this->assertSame(
+            realpath(dirname(__FILE__) . '/resources/databases/dbName/data/tableName.csv'),
+            $pathFinder->findDataPath(array('csv'))
+        );
+    }
+
+    /**
+     * Tests when there is no file.
+     */
+    public function testFindDataPathWithoutFileFounded()
+    {
+        $pathFinder = new PathFinder($this->getTable('noTableName'));
+        $this->setExpectedException('\RuntimeException');
+        $pathFinder->findDataPath(array('csv'));
+    }
+
+    /**
+     * Tests when there is a file, but when the file is situated in the parent resources.
+     */
+    public function testFindDataPathWithFileFoundedForTheParent()
+    {
+        $pathFinder = new PathFinder($this->getTable('tableName2'));
+        $this->assertSame(
+            realpath(dirname(__FILE__) . '/../resources/databases/dbName/data/tableName2.csv'),
+            $pathFinder->findDataPath(array('csv'))
+        );
+    }
+
+    /**
+     * @param $tableName
+     * @return Table
+     */
+    private function getTable($tableName)
+    {
+        return new Table($this->getTest(), 'dbName', $tableName);
+    }
+
+    /**
+     * @return Test
+     */
+    private function getTest()
+    {
+        return new Test(new MethodReflection(__CLASS__, __FUNCTION__));
+    }
+}
+ 
