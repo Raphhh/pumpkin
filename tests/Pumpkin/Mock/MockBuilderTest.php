2fb386b81c3c6bd33cf6c92a688323dcf3a4cd57
Raphaël Lefebvre <raphael@raphaellefebvre.be>
2014-05-25 18:13:01 +0200

    init MockBuilder

---
+++ tests/Pumpkin/Mock/MockBuilderTest.php
@@ -0,0 +1,16 @@
+<?php
+namespace Pumpkin\Mock;
+
+/**
+ * Class MockBuilderTest
+ * @package Pumpkin\Mock
+ * @author Raphaël Lefebvre <raphael@raphaellefebvre.be>
+ */
+class MockBuilderTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetMocks()
+    {
+
+    }
+}
+ 
